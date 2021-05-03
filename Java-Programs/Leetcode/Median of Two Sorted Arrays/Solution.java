0a34d3d6df996ceb61de9a9c344206fe4b38f27d
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-05-03 14:13:56 +0530

    Update Solution.java

--- Java-Programs/Leetcode/Median of Two Sorted Arrays/Solution.java
+++ Java-Programs/Leetcode/Median of Two Sorted Arrays/Solution.java
@@ -1,3 +1,6 @@
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+    
+    
 import java.util.*;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
