6a5d92e1d9019182eef01fcabb4c90dab6fb6909
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-05-03 14:07:38 +0530

    Create Solution.java

---
+++ Java-Programs/Leetcode/Median of Two Sorted Arrays/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int a[]=new int[(nums1.length)+(nums2.length)];
+        int k=0,index=0;
+        double median=0;
+        for(int i=0;i<nums1.length;i++)
+        {
+            a[k++]=nums1[i];
+        }
+        for(int j=0;j<nums2.length;j++){
+            a[k++]=nums2[j];}
+        Arrays.sort(a);
+        index = k/2;
+        if(k%2 == 0){
+           median = ((a[index-1]+a[index])/2);
+           if(((a[index-1]+a[index])%2)==1)
+               median+=0.5;}
+           
+        else
+            median = a[index];
+        
+        return median;
+         
+            
+    }
+}
