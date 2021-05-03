1a9f2d6c8fdc63695dfb4166d4a8b5a3c022fe4f
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-05-03 14:15:07 +0530

    Update Solution.java

--- Java-Programs/Leetcode/Shortest Palindrome/Solution.java
+++ Java-Programs/Leetcode/Shortest Palindrome/Solution.java
@@ -1,3 +1,18 @@
+// You are given a string s. You can convert s to a palindrome by adding characters in front of it.
+
+// Return the shortest palindrome you can find by performing this transformation.
+
+ 
+
+// Example 1:
+
+// Input: s = "aacecaaa"
+// Output: "aaacecaaa"
+// Example 2:
+
+// Input: s = "abcd"
+// Output: "dcbabcd"
+    
 class Solution {
     public String shortestPalindrome(String s) {
         int i = 0,end = s.length()-1,j = end;
