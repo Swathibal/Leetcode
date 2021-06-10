082b0955de4ee90b65dbbdc542cc39dae293b999
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-06-10 17:04:54 +0530

    Create Solution.java

---
+++ Java-Programs/Leetcode/145. Binary Tree Postorder Traversal/Solution.java
@@ -0,0 +1,52 @@
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+// Example 1:
+
+// Input: root = [1,null,2,3]
+// Output: [3,2,1]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [1]
+// Output: [1]
+// Example 4:
+
+
+// Input: root = [1,2]
+// Output: [2,1]
+// Example 5:
+
+
+// Input: root = [1,null,2]
+// Output: [2,1]
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> l = new ArrayList<Integer>();
+        if(root == null)
+            return l;
+        l.addAll(postorderTraversal(root.left));
+        l.addAll(postorderTraversal(root.right));
+        l.add(root.val);
+        
+        return l;
+    }
+}
