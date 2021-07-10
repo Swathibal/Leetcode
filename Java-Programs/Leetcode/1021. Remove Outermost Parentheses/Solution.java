81ab8f934679a4077fd9d6ccf3cb6ef2e440c3c2
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-07-10 19:46:33 +0530

    Update Solution.java

--- Java-Programs/Leetcode/1021. Remove Outermost Parentheses/Solution.java
+++ Java-Programs/Leetcode/1021. Remove Outermost Parentheses/Solution.java
@@ -66,3 +66,31 @@ class Solution {
         return string;
     }
 }
+
+(or)
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        String str="";
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                if(count!=0)
+                    str+=s.charAt(i);
+                
+                count++;
+            }
+            else
+            {
+                count--;
+                if(count!=0)
+                    str+=s.charAt(i);
+                
+                
+            }
+        }
+        return str;
+    }
+}
