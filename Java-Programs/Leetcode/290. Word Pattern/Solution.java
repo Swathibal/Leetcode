04d6a360d1901b3a6ff29f44576f6df3efc4b5c9
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-07-11 23:43:09 +0530

    Create Solution.java

---
+++ Java-Programs/Leetcode/290. Word Pattern/Solution.java
@@ -0,0 +1,54 @@
+// Given a `pattern` and a string `s`, find if `s` follows the same pattern.
+
+// Here **follow** means a full match, such that there is a bijection between a letter in `pattern` and a **non-empty** word in `s`.
+
+// **Example 1:**
+
+// ```
+// Input: pattern = "abba", s = "dog cat cat dog"
+// Output: true
+
+// ```
+
+// **Example 2:**
+
+// ```
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+
+// ```
+
+// **Example 3:**
+
+// ```
+// Input: pattern = "aaaa", s = "dog cat cat dog"
+// Output: false
+
+// ```
+
+// **Example 4:**
+
+// Input: pattern = "abba", s = "dog dog dog dog"
+// Output: false
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+       String[] arr= s.split(" ");
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        if(arr.length!= pattern.length())
+            return false;
+        for(int i=0; i<arr.length; i++){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(arr[i]))
+                    return false;
+            }else{
+                if(map.containsValue(arr[i]))
+                    return false;
+                map.put(c, arr[i]);
+            }    
+        }
+        return true;
+    
+    }
+}
