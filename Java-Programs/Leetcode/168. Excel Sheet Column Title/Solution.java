7f2eaeb4a0f4f31479ff3ae1da12f3fe55ac3bc4
Swathibal <66405048+Swathibal@users.noreply.github.com>
2021-07-14 22:22:03 +0530

    Create Solution.java

---
+++ Java-Programs/Leetcode/168. Excel Sheet Column Title/Solution.java
@@ -0,0 +1,47 @@
+// Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+// For example:
+
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28 
+// ...
+
+ 
+
+// Example 1:
+
+// Input: columnNumber = 1
+// Output: "A"
+
+// Example 2:
+
+// Input: columnNumber = 28
+// Output: "AB"
+
+// Example 3:
+
+// Input: columnNumber = 701
+// Output: "ZY"
+
+// Example 4:
+
+// Input: columnNumber = 2147483647
+// Output: "FXSHRXW
+
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder s = new StringBuilder();
+        while(columnNumber>0)
+        {
+            char c = (char)('A' + ((columnNumber-1)%26));
+            s.append(c);
+            columnNumber = (columnNumber-1)/26;
+        }
+        return s.reverse().toString();
+    }
+}
